7070a5535504108e75bfe76bc8087d1b6ad7701c
akashbroo007 <akashfgz80@gmail.com>
2026-04-19 02:32:37 +0530

    fix: remove ScrollEdgeHelper and simplify CSS to restore article list scrolling

--- src/app/studio/layout.tsx
+++ src/app/studio/layout.tsx
@@ -1,12 +1,10 @@
 import type React from 'react'
 
 import './studio.css'
-import ScrollEdgeHelper from './ScrollEdgeHelper'
 
 export default function StudioLayout({children}: {children: React.ReactNode}) {
   return (
     <div className="sanity-studio-scope">
-      <ScrollEdgeHelper />
       {children}
     </div>
   )
